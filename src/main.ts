40a353c73b019e4ca31735fcb92ab10663c3e9a4
Renan Nardi <renan.nardi.dev@gmail.com>
2025-10-22 13:22:50 -0300

    cors .env

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
 
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: process.env.CORS || 'http://localhost:3001',
     credentials: true,
   });
 
